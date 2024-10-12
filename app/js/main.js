8b1f146323e3c8884b57e65c6285c276aad64749
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-10-12 19:24:00 +0300

    - hotfix catalog - change section home-catalog

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,28 @@ new Swiper('.start__swiper', {
     }
   }
 });
+
+new Swiper('.home-catalog__swiper', {
+  speed: 600,
+  spaceBetween: 10,
+  navigation: {
+    prevEl: '.home-catalog__btn--prev',
+    nextEl: '.home-catalog__btn--next'
+  },
+  keyboard: {
+    enabled: true
+  },
+  breakpoints: {
+    240: {
+      slidesPerView: 'auto'
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 15
+    },
+  }
+});
